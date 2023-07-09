c4ba5f3cac0e27170284c4e05eab7246a3b70c61
mmdctjj <jiangpengjie@bizseer.com>
2023-07-09 23:50:42 +0800

    feat: add homepage

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 export default {
   input: 'src/index.js', // 入口文件路径
   output: {
-    file: 'dist/bundle.js', // 输出文件路径
+    file: 'dist/index.js', // 输出文件路径
     format: 'es', // 输出格式（可选：amd, cjs, es, iife, umd）
     name: 'rollup-plugin-remove-others-console' // 输出的UMD模块名称
   }
