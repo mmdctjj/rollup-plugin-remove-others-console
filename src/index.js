22fdb7ba99a028621909783117d17d44eae9d89f
mmdctjj <jiangpengjie@bizseer.com>
2024-11-03 22:55:11 +0800

    feat: 🚀 优化代码逻辑，使用 ast 替换正则表达式

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import childProcess from 'child_process';
-import { type } from 'os';
-
-// 判断平台，win平台不支持grep
-const isWin = type() === 'Windows_NT'
-const findStr = isWin ? 'findstr' : 'grep'
+import { parse } from "@babel/parser"
+import generate from "@babel/generator"
+import t from "@babel/types";
+import traverse from "@babel/traverse"
 
 const userName = childProcess.execSync(
   `git config user.name`,
@@ -17,25 +16,70 @@ const RollupPluginRemoveOthersConsole = () => {
     enforce: "pre",
     transform: (code, id) => {
       if (!id.includes('node_modules') && code.includes(`console.log(`)) {
-        const rows = code.split('\n')
+        
+        // 如果当前文件没有当前作者，就全局替换，无需使用 ast
+        const authorList = childProcess.execSync(`git log --pretty="%an" -- ${id}`, {
+          encoding: 'utf-8',
+        });
 
-        const includesLines = rows.map((row, idx) => row.includes(`console.log(`) ? idx : undefined).filter(n => n)
+        if (!authorList.includes(userName)) {
+          
+          return code.replace(
+            /console\.log\((?:[^)(]|\((?:[^)(]+|\([^)(]*\))*\))*\)[;\n]?/g,
+            '{}\n',
+          );
+        }
 
-        const removeLine = includesLines.filter(line => {
-          const authorInfo = childProcess.execSync(
-            `git blame -L ${line+1},${line+1} --porcelain ${id} | ${findStr} "^author "`,
-            { encoding: 'utf-8' }
-          )
-          const author = authorInfo.slice(authorInfo.indexOf(`author `) + 7)
-          return ![userName, `Not Committed Yet`].includes(author)
-        })
+        // 初始化 ast
+        const ast = parse(code, {
+          sourceType: 'module',
+          plugins: ['jsx', 'tsx', 'vue', 'typescript', 'classProperties'],
+        });
 
-        return rows.map((row, idx) => {
-          if (removeLine.includes(idx)) {
-            return row.replace(/console\.log\((?:[^)(]|\((?:[^)(]+|\([^)(]*\))*\))*\)[;\n]?/g, `{}`)
+        const consoleLogLines = [];
+
+        const visitor = {
+          CallExpression(path) {
+            if (
+              t.isMemberExpression(path.node.callee) &&
+              t.isIdentifier(path.node.callee.object, { name: 'console' }) &&
+              t.isIdentifier(path.node.callee.property, { name: 'log' })
+            ) {
+              const line = path.node.loc.start.line;
+              consoleLogLines.push(line);
+            }
+          },
+        };
+
+        traverse(ast, visitor);
+
+        const removeLines = consoleLogLines.filter((line) => {
+          const authorInfo = childProcess.execSync(`git blame -L ${line},${line} --porcelain ${id}`, {
+            encoding: 'utf-8',
+          });
+          const authorMatch = authorInfo.match(/^author (.*)$/m);
+          const author = authorMatch ? authorMatch[1].trim() : '';
+          if (author !== userName && author !== 'Not Committed Yet') {
+            return true;
           }
-          return row
-        }).join('\n')
+          return false;
+        });
+
+         // 遍历AST并替换console.log
+        traverse(ast, {
+          CallExpression(path) {
+            if (removeLines.includes(path.node.loc.start.line)) {
+              const emptyObjectExpression = t.objectExpression([]);
+              // 设置空对象的 trailingComments 来保留换行效果
+              emptyObjectExpression.trailingComments = [{ type: "CommentLine", value: "" }];
+              path.replaceWith(emptyObjectExpression);
+            }
+          },
+        });
+
+        result = generate(ast).code;
+
+        return result;
       }
       return code
     }
